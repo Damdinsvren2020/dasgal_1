72b200a83299a1e79258c56172747273354899cf
Damdinsvren2020 <56712755+Damdinsvren2020@users.noreply.github.com>
2022-06-27 19:05:32 +0800

    project

---
+++ app.js
@@ -0,0 +1,26 @@
+// value utga
+
+// 33;
+// 123;
+// 13.4;
+
+// Number too
+
+// "Hello world"  String type
+
+// '' single quote
+// "" double quote
+
+// true             vnen
+// false  Boolean   hudal
+
+// [1,3,6] array buyu hvsnegt daraalaltai ugugdliig hadgaldag 0 ees ehelj toolno
+// {name:"bat"} object
+
+// Null хоосон
+
+// Undefined бүр байхгүй хоосон утга
+
+let x = 3.14;
+let y = 3;
+document.getElementById("demo").innerHTML = x + "<br>" + y;
